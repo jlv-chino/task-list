e8be3c93c4e9c35ab4ac3ba6e333b7fb6b66b0d6
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-02-07 02:06:01 -0300

    Primer botón

---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Input() text: string = "";
+  @Input() color: string = "";
+  @Output() btnClick = new EventEmitter();
+
+  constructor(){
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onClick(){
+    this.btnClick.emit(this.text);
+  }
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -17,4 +17,8 @@ export class HeaderComponent{
 
   }
 
+  toggleAddTask(){
+    
+  }
+
 }
