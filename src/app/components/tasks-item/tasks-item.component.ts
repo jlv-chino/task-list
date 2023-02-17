ff0afd4a17c8912b466b6d78d20e7edc0b777b0a
José Luis Vilches <jose.luis.comvilches@gmail.com>
2023-02-17 01:49:41 -0300

    lista de items

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-tasks';
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent {
+  @Input() task: Task = TASKS[0];
+
+}
